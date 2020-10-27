aef681963ea2eb6a8140aa18f221cb8d28e55147
onurmatik <onurmatik@gmail.com>
2020-10-27 12:03:32 +0300

    expand network

--- oep/network/views.py
+++ oep/network/views.py
@@ -1,17 +1,33 @@
 from django.shortcuts import render
 from django import forms
+from django.utils.translation import ugettext as _
 from oep.network.models import Map, RelationType, RELATION_GROUPS, ORGANIZATION_SIZES
 
 
-class EntityForm(forms.Form):
-    name = forms.CharField()
-    size = forms.ChoiceField(choices=ORGANIZATION_SIZES)
-    relation_type_choices = []
+def relation_type_choices():
+    choices = []
     for group_code, group_name in RELATION_GROUPS:
-        relation_type_choices.append(
+        choices.append(
             (group_name, [(rt.id, rt.name) for rt in RelationType.objects.filter(group=group_code)])
         )
+    return choices
+
+
+def add_to_choices():
+    choices = [(0, _('Yourself'))]
+    return choices
+
+
+class EntityForm(forms.Form):
+    add_to = forms.ChoiceField(label=_('Relate to'), choices=add_to_choices)
+    name = forms.CharField(label=_('Name of the entity'))
+    size = forms.ChoiceField(label=_('Size of the entity'), choices=ORGANIZATION_SIZES)
     relation_type = forms.ChoiceField(choices=relation_type_choices)
+    weight = forms.ChoiceField(label=_('We interact'), choices=(
+        (3, _('Regularly')),
+        (2, _('Sometimes')),
+        (1, _('Rarely')),
+    ))
 
 
 def graph(request):
