7743c49ede4198a21e2aca5588d38a245a2ad2f8
onurmatik <onurmatik@gmail.com>
2020-11-04 15:48:48 +0300

    mapper updates

--- oep/mapper/views.py
+++ oep/mapper/views.py
@@ -1,5 +1,6 @@
 import json
 from collections import defaultdict
+import networkx as nx
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import Http404
 from django import forms
@@ -76,14 +77,111 @@ class OrganisationForm(forms.Form):
                     'size': 3,
                     'color': "#f00",
                 }],
-                'edges': [],
             }
         })
         request.session['data'] = data
 
 
-class StakeholderNameForm(forms.Form):
-    name = forms.CharField(label=_('Name of the stakeholder'))
+class StakeholderForm(forms.Form):
+    customers = forms.CharField(
+        label='Who are our key customers?',
+        help_text='In other words, who receives our products, whose needs do we serve?',
+        widget=forms.Textarea(),
+    )
+    supliers = forms.CharField(
+        label='Who are our key suppliers?',
+        help_text='In other words, who do we receive materials or resources from?',
+        widget=forms.Textarea(),
+    )
+    collaborators = forms.CharField(
+        label='Who are our key collaborators?',
+        help_text='In other words, who do we develop our offerings, operations, or other solutions with?',
+        widget=forms.Textarea(),
+    )
+    supporters = forms.CharField(
+        label='Who are our key supporters?',
+        help_text='In other words, who make our work possible or easier?',
+        widget=forms.Textarea(),
+    )
+
+    def save(self, request, cleaned_data):
+        data = request.session.get('data', {})
+        data.update(cleaned_data)
+        nodes = data['graph']['nodes']
+        edges = data['graph'].get('edges', [])
+        node_id = nodes[-1]['id'] + 1
+        for stakeholder_type in cleaned_data.keys():
+            for name in cleaned_data[stakeholder_type].split(','):
+                name = name.strip()
+                nodes.append({
+                    'id': node_id,
+                    'label': name,
+                    'x': 0, 'y': 0,
+                    'size': 3,
+                    'color': "#f00",
+                })
+                edges.append({
+                    'id': 'e' + str(node_id),
+                    'source': 0,
+                    'target': node_id,
+                    'size': 1,
+                    'label': stakeholder_type,
+                    'color': '#fff',
+                })
+                node_id += 1
+        data.update({
+            'graph': {
+                'nodes': nodes,
+                'edges': edges,
+            }
+        })
+        request.session['data'] = data
+        request.session.modified = True
+
+
+class StakeholderExtraForm(forms.Form):
+    extra = forms.CharField(
+        label='Anyone or any organization you’d like to add to your stakeholder map right away?',
+        widget=forms.Textarea(),
+    )
+
+    def save(self, request, cleaned_data):
+        data = request.session.get('data', {})
+        data.update(cleaned_data)
+        nodes = data['graph']['nodes']
+        edges = data['graph'].get('edges', [])
+        G = nx.path_graph(len(nodes))
+        positions = list(nx.circular_layout(G, scale=100).values())
+        node_id = nodes[-1]['id'] + 1
+        for stakeholder_type in cleaned_data.keys():
+            for name in cleaned_data[stakeholder_type].split(','):
+                position = positions.pop()
+                name = name.strip()
+                nodes.append({
+                    'id': node_id,
+                    'label': name,
+                    'x': position[0],
+                    'y': position[1],
+                    'size': 3,
+                    'color': "#f00",
+                })
+                edges.append({
+                    'id': 'e' + str(node_id),
+                    'source': 0,
+                    'target': node_id,
+                    'size': 1,
+                    'label': stakeholder_type,
+                    'color': '#fff',
+                })
+                node_id += 1
+        data.update({
+            'graph': {
+                'nodes': nodes,
+                'edges': edges,
+            }
+        })
+        request.session['data'] = data
+        request.session.modified = True
 
 
 def relation_type_choices():
@@ -189,16 +287,51 @@ PAGES = {
     1: {
         'template': 'mapper/form.html',
         'context': {
+            'description': 'Let’s get you set up. To support you in identifying and selecting potential collaborators, '
+                           'let’s start by getting to know your organisation or team.',
             'form': OrganisationForm,
         },
     },
     2: {
         'template': 'mapper/map.html',
         'context': {
-            'description': '''
-                <p>Congratulations, you successfully created your stakeholder map!</p>
-                <p>It’s looking rather empty here though, are you ready to add some key stakeholders?</p>
-            ''',
+            'description': '<p>Congratulations, you successfully created your stakeholder map!</p>'
+                           '<p>It’s looking rather empty here though, are you ready to add some key stakeholders?</p>',
+        },
+    },
+    3: {
+        'template': 'mapper/form.html',
+        'context': {
+            'title': 'Add stakeholders',
+            'description': "Let's start by adding some usual suspects. "
+                           "Try to name at least three key stakeholders per question. "
+                           "Separate stakeholders with commas, press tab to move to the next question.",
+            'form': StakeholderForm,
+        },
+    },
+    4: {
+        'template': 'mapper/form.html',
+        'context': {
+            'title': 'Add stakeholders',
+            'description': "For food and beverage SMEs, we’ve found 6 groups of development influencers and collaborators."
+                           "<ul>"
+                               "<li>customers and consumer communities,</li>"
+                               "<li>other SMEs in the region or product niche,</li>"
+                               "<li>the wider food and beverage ecosystem (e.g. chefs, cafes),</li>"
+                               "<li>the supply and distribution chain (e.g. farmers, importers, retail chains),</li>"
+                               "<li>other organizations (eg public funders, universities, associations, companies "
+                                   "in other industries such as eg cosmetics or clothing), and</li>"
+                               "<li>personal networks (friends, family, hobbies)</li>"
+                           "</ul>",
+            'form': StakeholderExtraForm,
+        },
+    },
+    5: {
+        'template': 'mapper/map.html',
+        'context': {
+            'description': "Awesome, look at all your stakeholders floating around you! "
+                           "I’m sure you relate to them in different ways though, "
+                           "are you ready to describe the relations to these stakeholders?",
         },
     },
 }
@@ -222,8 +355,14 @@ def view_page(request, page_no, workshop_slug=None):
                 page['context'].update({
                     'form': form,
                 })
+    next_page = None
+    if not page['context'].get('form'):
+        next_page = page_no + 1
+        if not next_page in PAGES:
+            next_page = None
     page['context'].update({
-        'data': request.session.get('data', {})
+        'data': request.session.get('data', {}),
+        'next_page': next_page,
     })
     if workshop_slug:
         page['context'].update({
