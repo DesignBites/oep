d2151cc4a85309886368ac37460eb520555039ef
onurmatik <onurmatik@gmail.com>
2020-11-06 13:38:02 +0300

    restored code

--- oep/mapper/views.py
+++ oep/mapper/views.py
@@ -67,7 +67,6 @@ class OrganisationForm(forms.Form):
 
     def save(self, request, cleaned_data):
         request.session['map'] = cleaned_data
-        request.session.modified = True
 
 
 class StakeholderForm(forms.Form):
