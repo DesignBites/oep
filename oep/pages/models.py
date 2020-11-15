47035024ab6acab1ea500376a67802bc49db044c
onurmatik <onurmatik@gmail.com>
2020-11-15 19:05:47 +0300

    UI updates

--- oep/pages/models.py
+++ oep/pages/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.utils import timezone
-from modelcluster.fields import ParentalKey
 from wagtail.core.models import Page, Orderable, ClusterableModel
 from wagtail.core.fields import RichTextField, StreamField
 from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel, StreamFieldPanel
@@ -57,6 +55,7 @@ class AboutPage(Page):
             ('phone', blocks.CharBlock(required=False)),
         ])),
     ], blank=True)
+    footer = RichTextField(blank=True, null=True)
 
     max_count = 1
 
@@ -67,16 +66,37 @@ class AboutPage(Page):
             FieldPanel('text'),
         ]),
         StreamFieldPanel('team_members'),
+        FieldPanel('footer'),
     ]
 
 
-class Podcast(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField(blank=True, null=True)
-    url = models.URLField()
+class PodcastsPage(Page):
+    photo = models.ForeignKey(
+        'wagtailimages.Image', blank=True, null=True,
+        on_delete=models.SET_NULL, related_name='+',
+        verbose_name='Background photo',
+    )
+    header = RichTextField(max_length=500)
+    text = RichTextField()
+    podcasts = StreamField([
+        ('podcast', blocks.StructBlock([
+            ('title', blocks.CharBlock()),
+            ('thumbnail', ImageChooserBlock(required=False)),
+            ('description', blocks.RichTextBlock(required=False)),
+            ('url', blocks.URLBlock(required=False)),
+        ]))
+    ], blank=True)
 
-    def __str__(self):
-        return self.title
+    max_count = 1
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel([
+            ImageChooserPanel('photo'),
+            FieldPanel('header'),
+            FieldPanel('text'),
+        ]),
+        StreamFieldPanel('podcasts'),
+    ]
 
 
 class Event(models.Model):
