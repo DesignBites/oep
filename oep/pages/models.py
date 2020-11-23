734b31a66fff993b70b498fba88a11155f4b2229
onurmatik <onurmatik@gmail.com>
2020-11-23 11:19:15 +0300

    external links target _blank

--- oep/pages/models.py
+++ oep/pages/models.py
@@ -5,6 +5,27 @@ from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.images.blocks import ImageChooserBlock
 from wagtail.core import blocks
+from django.utils.html import escape
+from wagtail.core import hooks
+from wagtail.core.rich_text import LinkHandler
+
+
+class NewWindowExternalLinkHandler(LinkHandler):
+    # This specifies to do this override for external links only.
+    # Other identifiers are available for other types of links.
+    identifier = 'external'
+
+    @classmethod
+    def expand_db_attributes(cls, attrs):
+        href = attrs["href"]
+        # Let's add the target attr, and also rel="noopener" + noreferrer fallback.
+        # See https://github.com/whatwg/html/issues/4078.
+        return '<a href="%s" target="_blank" rel="noopener noreferrer">' % escape(href)
+
+
+@hooks.register('register_rich_text_features')
+def register_external_link(features):
+    features.register_link_type(NewWindowExternalLinkHandler)
 
 
 class HomePage(Page):
